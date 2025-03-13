7342699411d028765f163a64724e71ec9ae24f93
Willian Pinheiro <willpinhaaa@gmail.com>
2025-03-13 10:10:31 -0300

    add infra package for server start and exit handling

---
+++ infra/server.go
@@ -0,0 +1,63 @@
+package infra
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+const (
+	ServerAddress   = ":8000"
+	ShutdownTimeout = 5 * time.Second
+)
+
+func listenAndServe(server *http.Server) {
+	log.Printf("Starting server at address %s\n", server.Addr)
+
+	err := server.ListenAndServe()
+
+	switch {
+	case errors.Is(err, http.ErrServerClosed):
+		log.Println("Server closed")
+	default:
+		log.Fatalf("Error starting server: %s\n", err)
+	}
+}
+
+func handleShutdown(server *http.Server) {
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit // Wait for some syscall errors
+
+	log.Println("Shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), ShutdownTimeout)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatalf("Server could not shutdown gracefully: %v\n", err)
+	}
+
+	log.Println("Server exiting")
+}
+
+// Listen and serve a http.Handler (for example, chi.NewRouter()) and handles server shutdown
+func RunServer(port int, handler http.Handler) {
+	addr := fmt.Sprintf(":%d", port)
+
+	server := &http.Server{
+		Addr:    addr,
+		Handler: handler,
+	}
+
+	// Starts server in goroutine so that it won't block the shutdown handling below
+	go listenAndServe(server)
+
+	handleShutdown(server)
+}
